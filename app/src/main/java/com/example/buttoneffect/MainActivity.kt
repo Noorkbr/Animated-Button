0aa8a0e00bb607a4a94716b0f51172e01e96b51d
Noorkbr <freelancer.noorkbr@gmail.com>
2024-05-30 16:45:17 +0600

    new button test commit

--- app/src/main/java/com/example/buttoneffect/MainActivity.kt
+++ app/src/main/java/com/example/buttoneffect/MainActivity.kt
@@ -8,22 +8,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         val lightningButton: Button = findViewById(R.id.lightning_button)
+
 
         val lightningEffect = AnimationUtils.loadAnimation(this, R.anim.lightning_effect)
 
+        val myButton: MaterialButton = findViewById(R.id.my_button)
+
+        myButton.elevation = 8f
+
         lightningButton.setOnClickListener {
             it.startAnimation(lightningEffect)
-
+            myButton.setOnClickListener {
+                it.startAnimation(lightningEffect)
 
 
             }
         }
-    }
+    }}
 
